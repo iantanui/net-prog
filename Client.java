4fb3485c4b55f8207d2c31c215293608efd728a5
iantanui <rutooian@gmail.com>
2024-07-05 19:57:16 +0300

    java

---
+++ Client.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        String hostname = "localhost";
+        int port = 12345;
+
+        try (Socket socket = new Socket(hostname, port)) {
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            String message = "Hello Server";
+            writer.println(message);
+
+            String response = reader.readLine();
+            System.out.println("Server response: " + response);
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+            System.out.println("Server is on");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                String message = reader.readLine();
+                System.out.println("Received message from client: " + message);
+
+                writer.println("Server received: " + message);
+                
+                socket.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
